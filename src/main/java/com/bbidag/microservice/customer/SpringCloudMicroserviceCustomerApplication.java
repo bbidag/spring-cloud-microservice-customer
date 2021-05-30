446eb05746247862d39c7a89e563e76694f2d47a
bbidag <skawlgh112@gmail.com>
2021-05-30 17:55:36 +0900

    Initial commit

---
+++ src/main/java/com/bbidag/microservice/customer/SpringCloudMicroserviceCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.bbidag.microservice.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMicroserviceCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMicroserviceCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbidag/microservice/customer/SpringCloudMicroserviceCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbidag.microservice.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMicroserviceCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
